ea96e212b28f067a84a651f658d4586f0b22e18f
PIERLUIGI VITI <pierluigi@commercelayer.io>
2026-03-26 11:55:23 +0100

    fix: fix tests

--- test/commands/microstore/index.test.ts
+++ test/commands/microstore/index.test.ts
@@ -1,11 +1,10 @@
-import { expect, test } from '@oclif/test'
+import { runCommand } from '@oclif/test'
+import { expect } from 'chai'
+
 
 describe('microstore:index', () => {
-  test
-    .stdout()
-    .command(['microstore:noc'])
-    .it('runs NoC', ctx => {
-      expect(ctx.stdout).to.contain('-= NoC =-')
-    })
-
+  it('runs NoC', async () => {
+    const { stdout } = await runCommand<{ name: string }>(['microstore:noc'])
+    expect(stdout).to.contain('-= NoC =-')
+  }).timeout(15000)
 })
